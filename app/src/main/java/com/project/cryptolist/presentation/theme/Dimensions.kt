feffacde404c0a5ed4377aadde634b26d732b7c6
Denny <denny.shek@dfiretailgroup.com>
2024-03-17 01:11:20 +0800

    chore(presentation): centralise theme style of component

---
+++ app/src/main/java/com/project/cryptolist/presentation/theme/Dimensions.kt
@@ -0,0 +1,13 @@
+package com.project.cryptolist.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+val DividerHeight = 0.5.dp
+val PaddingSmall = 4.dp
+val PaddingMedium = 8.dp
+val PaddingLarge = 16.dp
+
+val PaddingMax = 40.dp
+
+val LetterIconSize = 30.dp
+val LetterIconRadius = 14.dp
---
+++ app/src/main/java/com/project/cryptolist/presentation/theme/FontSize.kt
@@ -0,0 +1,7 @@
+package com.project.cryptolist.presentation.theme
+
+import androidx.compose.ui.unit.sp
+
+val LargeFont = 24.sp
+val MediumFont = 18.sp
+val SmallFont = 14.sp
